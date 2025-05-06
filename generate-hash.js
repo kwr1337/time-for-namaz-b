4e58f3ed740a2f839453df0a51d378365085386f
kwr1337 <rostft@gmail.com>
2025-05-06 21:08:11 +0300

    1

--- generate-hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function generateHash() {
-  const password = 'admin';
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log('Хеш пароля "admin":', hashedPassword);
-}
-
-generateHash(); 
