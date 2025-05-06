26d1a451664c0ab776403fee39146a6f2bb5f71a
kwr1337 <rostft@gmail.com>
2025-05-06 17:55:37 +0300

    2

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['http://localhost:3000', 'https://time2namaz.alfasoftware.webtm.ru'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
