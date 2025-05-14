00472dbc06929505df34bd0c7943cba806ccc4a1
kwr1337 <rostft@gmail.com>
2025-05-14 14:26:46 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -3,68 +3,40 @@ import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 import { AppModule } from './app.module'
 import { join } from 'path'
-import { ValidationPipe } from '@nestjs/common'
-import * as fs from 'fs'
 
 // Импорт Swagger модулей
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	console.log('Запуск приложения...');
-	const app = await NestFactory.create(AppModule, {
-		logger: ['error', 'warn', 'log', 'debug', 'verbose'],
-	})
+	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: true,
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		origin: ['http://localhost:3000', 'https://time2namaz.alfasoftware.webtm.ru'],
 		credentials: true,
+		exposedHeaders: 'set-cookie'
 	})
 	app.use('/uploads', express.static(join(__dirname, '..', 'uploads')))
 
-	// Включаем валидацию
-	app.useGlobalPipes(new ValidationPipe())
-
 	// Конфигурация Swagger
 	const config = new DocumentBuilder()
-		.setTitle('Time For Namaz API')
-		.setDescription('API для приложения Time For Namaz')
+		.setTitle('Название API')
+		.setDescription('Описание API')
 		.setVersion('1.0')
 		.addBearerAuth() // Добавление поддержки Bearer токенов, если нужно
 		.build()
 
 	// Создание документа и настройка Swagger по пути /api-docs
 	const document = SwaggerModule.createDocument(app, config)
-	SwaggerModule.setup('api', app, document)
+	SwaggerModule.setup('api-docs', app, document)
 
-	// Определяем порт
+	// Используем стандартный порт 8080 для Node.js приложений в облаке
 	const PORT = 8080;
-
-	// Создаем файл с портом для Timeweb Cloud
-	try {
-		fs.writeFileSync('.port', PORT.toString())
-		console.log('Файл .port создан успешно')
-	} catch (error) {
-		console.error('Ошибка при создании файла .port:', error)
-	}
-
-	console.log(`Запуск сервера на порту ${PORT}...`);
-	
-	await app.listen(PORT, '0.0.0.0', () => {
-		console.log(`Сервер успешно запущен на порту ${PORT}`);
-		console.log('Настройки CORS:', app.getHttpAdapter().getInstance()._cors);
-	})
+	console.log(`Приложение запускается на порту: ${PORT}`);
+	await app.listen(PORT);
+	console.log(`Приложение успешно запущено на порту ${PORT}`);
 }
-
-// Добавляем обработку необработанных исключений
-process.on('unhandledRejection', (reason, promise) => {
-	console.error('Необработанное отклонение промиса:', reason);
-});
-
-process.on('uncaughtException', (error) => {
-	console.error('Необработанное исключение:', error);
-});
 
 bootstrap()
